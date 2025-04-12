af70685e7fa2138450641dd2f0b1ee53c432c1d4
Darpan <darpan.kapadia@gmail.com>
2025-04-12 15:41:06 +1200

    Implemented game phases

--- src/main/kotlin/data/Room.kt
+++ src/main/kotlin/data/Room.kt
@@ -1,6 +1,8 @@
 package com.dkdev45.data
 
 import io.ktor.websocket.*
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.internal.synchronized
 import kotlinx.coroutines.isActive
 
 class Room(
@@ -8,6 +10,34 @@ class Room(
     val maxPlayers: Int,
     var players: List<Player> = listOf()
 ) {
+
+    private var phaseChangeListener:((Phase)-> Unit)? = null
+    @OptIn(InternalCoroutinesApi::class)
+    var phase = Phase.WAITING_FOR_PLAYERS
+        set(value) {
+            synchronized(field) {
+                field = value
+                phaseChangeListener?.let { change ->
+                    change(value)
+                }
+            }
+        }
+
+    private fun setPhaseChangeListener(listener: (Phase) -> Unit) {
+        phaseChangeListener = listener
+    }
+
+    init {
+        setPhaseChangeListener { newPhase ->
+            when(newPhase) {
+                Phase.WAITING_FOR_PLAYERS -> waitingForPlayers()
+                Phase.WAITING_FOR_START -> waitingForStart()
+                Phase.NEW_ROUND -> newRound()
+                Phase.GAME_RUNNING -> gameRunning()
+                Phase.SHOW_WORD -> showWord()
+            }
+        }
+    }
 
     suspend fun broadcast(message: String) {
         players.forEach {player ->
@@ -29,5 +59,34 @@ class Room(
         return players.find {
             it.username == username
         } != null
+    }
+
+    private fun waitingForPlayers() {
+
+    }
+
+    private fun waitingForStart(){
+
+    }
+
+    private fun newRound(){
+
+
+    }
+
+    private fun gameRunning() {
+
+    }
+
+    private fun showWord() {
+
+    }
+
+    enum class Phase {
+        WAITING_FOR_PLAYERS,
+        WAITING_FOR_START,
+        NEW_ROUND,
+        GAME_RUNNING,
+        SHOW_WORD
     }
 }
