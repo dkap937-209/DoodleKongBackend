38dc5f64f68307edda2a2c29acf15b779fa48466
Darpan <darpan.kapadia@gmail.com>
2025-05-09 21:33:50 +1200

    Removed default colours for CallLogging

--- src/main/kotlin/Monitoring.kt
+++ src/main/kotlin/Monitoring.kt
@@ -1,21 +1,14 @@
 package com.dkdev45
 
-import io.ktor.serialization.gson.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.calllogging.*
-import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.request.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-import io.ktor.server.websocket.*
-import io.ktor.websocket.*
-import java.time.Duration
-import kotlin.time.Duration.Companion.seconds
-import org.slf4j.event.*
+import org.slf4j.event.Level
 
 fun Application.configureMonitoring() {
     install(CallLogging) {
         level = Level.INFO
         filter { call -> call.request.path().startsWith("/") }
+        disableDefaultColors()
     }
 }
